cb55bc67fc5bf30f1662b7c059bcb573c55bd1d6
Steven Noonan <steven@uplinklabs.net>
2022-09-10 12:58:15 -0700

    allocator: ensure only one non-lazy instance of allocate/deallocate exists
    This fixes a bunch of link-time warnings with Apple Clang.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/meshoptimizer.h
+++ src/meshoptimizer.h
@@ -28,6 +28,9 @@
 #endif
 #endif
 
+/* IV: Always enable this for our code. */
+#define MESHOPTIMIZER_ALLOC_EXPORT
+
 /* Experimental APIs have unstable interface and might have implementation that's not fully tested or optimized */
 #ifndef MESHOPTIMIZER_EXPERIMENTAL
 #define MESHOPTIMIZER_EXPERIMENTAL MESHOPTIMIZER_API
@@ -38,6 +41,9 @@
 extern "C"
 {
 #endif
+
+typedef void* (MESHOPTIMIZER_ALLOC_CALLCONV *meshopt_alloc_t)(size_t);
+typedef void (MESHOPTIMIZER_ALLOC_CALLCONV *meshopt_dealloc_t)(void*);
 
 /**
  * Vertex attribute stream
@@ -1039,7 +1045,7 @@ MESHOPTIMIZER_EXPERIMENTAL int meshopt_computePositionExponent(const float* minv
  *
  * In shared library builds, allocations from templated index wrappers in this header will only be redirected if MESHOPTIMIZER_ALLOC_EXPORT is defined.
  */
-MESHOPTIMIZER_API void meshopt_setAllocator(void* (MESHOPTIMIZER_ALLOC_CALLCONV* allocate)(size_t), void (MESHOPTIMIZER_ALLOC_CALLCONV* deallocate)(void*));
+MESHOPTIMIZER_API void meshopt_setAllocator(meshopt_alloc_t, meshopt_dealloc_t);
 
 #ifdef __cplusplus
 } /* extern "C" */
@@ -1196,8 +1202,8 @@ class meshopt_Allocator
 public:
 	struct Storage
 	{
-		void* (MESHOPTIMIZER_ALLOC_CALLCONV* allocate)(size_t);
-		void (MESHOPTIMIZER_ALLOC_CALLCONV* deallocate)(void*);
+		meshopt_alloc_t allocate;
+		meshopt_dealloc_t deallocate;
 	};
 
 #ifdef MESHOPTIMIZER_ALLOC_EXPORT
