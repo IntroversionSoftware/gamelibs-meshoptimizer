3f0e827c8e99f036178a0a1c8d4ea9a7ffab4616
Steven Noonan <steven@uplinklabs.net>
2022-09-10 12:58:15 -0700

    allocator: ensure only one non-lazy instance of allocate/deallocate exists
    This fixes a bunch of link-time warnings with Apple Clang.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -1,6 +1,9 @@
 // This file is part of meshoptimizer library; see meshoptimizer.h for version/license details
 #include "meshoptimizer.h"
 
+meshopt_alloc_t meshopt_Allocator::Storage::allocate = operator new;
+meshopt_dealloc_t meshopt_Allocator::Storage::deallocate = operator delete;
+
 void meshopt_setAllocator(void*(MESHOPTIMIZER_ALLOC_CALLCONV* allocate)(size_t), void(MESHOPTIMIZER_ALLOC_CALLCONV* deallocate)(void*))
 {
 	meshopt_Allocator::Storage::allocate = allocate;
